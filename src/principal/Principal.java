af706cbc4907a5716dbb706ba0aefb327784697f
djmm <djmeridama@outlook.com>
2021-06-30 14:28:40 -0600

    main

---
+++ src/principal/Principal.java
@@ -0,0 +1,15 @@
+
+package principal;
+import bienvenida.Inicio;
+
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        
+    }
+    
+    
+}
